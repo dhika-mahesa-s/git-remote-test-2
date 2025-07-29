538cae99ee93ae878bf54eba4fc14f29a0400ee3
Dhika Mahesa S <dhikamahesa45@gmail.com>
2025-07-29 16:51:16 +0700

    Menambahkan list di index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia quod tenetur odit ab quis atque voluptatum eaque cupiditate, libero perferendis quae maxime voluptate laboriosam iste, accusantium ad, expedita qui id.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
