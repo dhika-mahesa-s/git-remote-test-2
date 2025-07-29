9f7d40030b7e0ee4f2d4f76f8f5fd4f31c9cfd02
Dhika Mahesa S <dhikamahesa45@gmail.com>
2025-07-29 16:32:36 +0700

    Menambahkan paragraf pada index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia quod tenetur odit ab quis atque voluptatum eaque cupiditate, libero perferendis quae maxime voluptate laboriosam iste, accusantium ad, expedita qui id.</p>
 </body>
 </html>
